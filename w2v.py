9d3cd798911b7b5141214c0eaa9b03a21fd551a9
guimatsumoto <guilhermekmatsumoto@gmail.com>
2017-11-09 11:16:39 +0100

    w2v file

---
+++ w2v.py
@@ -0,0 +1,21 @@
+# @guimatsumoto
+# @jesussanchez
+#
+# First implementation of a word2vec algorithm, on a skip-gram model.
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import math
+import os
+import random
+from tempfile import gettempdir
+import zipfile
+
+import numpy as np
+import tensorflow as tf
+from six.moves import urllib
+from six.moves import xrange
